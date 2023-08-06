a35f4e699406be62750c44506e6c877c1d7e7c6c
manjioko <643898155@qq.com>
2023-08-06 05:46:31 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <cmain />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import cmain from './components/mainUi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    cmain
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/mainUi.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+// import { ref } from 'vue'
+
+// const count = ref(0)
+
+</script>
+<template>
+    <main class="main">
+        <section class="chat-window">
+            
+        </section>
+    </main>
+</template>
+<style lang="scss" scoped>
+.main {
+    background-color: #F5F6FA;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.chat-window {
+    width: 70vw;
+    height: 85vh;
+    max-width: 1000px;
+    max-height: 875px;
+    background-color: #fff;
+}
+</style>
