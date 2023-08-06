60c128c5277f0f1344621d843492c5d6c5970843
Manjioko <zz6438@qq.com>
2023-08-06 21:52:17 +0800

    修改UI

--- src/components/mainUi.vue
+++ src/components/mainUi.vue
@@ -8,9 +8,16 @@
 <template>
     <main class="main">
         <section class="chat-window">
+            <section class="text-top">
+                <div class="avatar">
+                    <img src="../assets/avatar1.png" alt="avatar">
+                    <span>Manjioko🐶</span>
+                </div>
+                <img src="../assets/setting.png" alt="setting">
+            </section>
             <section class="text-show"></section>
             <section class="text-send">
-                <input type="text">
+                <input type="text" placeholder="在这里输入你的消息...">
                 <button>
                     <span>发送</span>
                     <img src="../assets/send.png" alt="send">
@@ -37,17 +44,24 @@
     background-color: #fff;
     display: flex;
     flex-direction: column;
+    border-radius: 5px;
+    box-shadow: 0px 1px 6px 6px rgba(221, 223, 230, 0.31);
 }
 .text-show {
     flex: 1;
 }
 .text-send {
-    height: 7vh;
+    height: 100px;
     display: flex;
     box-sizing: border-box;
-    padding: 8px;
+    padding: 25px 20px 25px 20px;
+    border-top: 2px solid #F5F6FA;
     input {
         flex: 1;
+        border: none;
+        outline: none;
+        font-size: 18px;
+        font-family: Source Han Sans CN;
     }
     button {
         width: 140px;
@@ -57,6 +71,9 @@
         justify-content: center;
         align-items: center;
         border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-family: Source Han Sans CN;
         span {
             font-size: 18px;
             color: #FFFFFF;
@@ -65,6 +82,32 @@
             width: 18px;
             height: 18px;
             margin-left: 12px;
+        }
+    }
+}
+.text-top {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 19px;
+    box-shadow: 0px 3px 6px 0px rgba(221,223,230,0.31);
+    font-family: Source Han Sans CN;
+    img {
+        width: 22px;
+        height: 22px;
+    }
+    .avatar {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        img {
+            width: 54px;
+            height: 54px;
+        }
+        span {
+            margin-left: 19px;
+            font-size: 30px;
+            color: #333333;
         }
     }
 }
