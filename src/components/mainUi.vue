e54effe76a6eb3caa44c9f13550c829e6203496a
manjioko <643898155@qq.com>
2023-08-06 06:14:01 +0000

    开始构建聊天界面

--- src/components/mainUi.vue
+++ src/components/mainUi.vue
@@ -8,7 +8,13 @@
 <template>
     <main class="main">
         <section class="chat-window">
-            
+            <section class="text-show"></section>
+            <section class="text-send">
+                <input type="text">
+                <button>
+                    <span>发送</span>
+                </button>
+            </section>
         </section>
     </main>
 </template>
@@ -24,8 +30,21 @@
 .chat-window {
     width: 70vw;
     height: 85vh;
+    min-width: 600px;
     max-width: 1000px;
     max-height: 875px;
     background-color: #fff;
+    display: flex;
+    flex-direction: column;
+}
+.text-show {
+    flex: 1;
+}
+.text-send {
+    height: 7vh;
+    display: flex;
+    input {
+        flex: 1;
+    }
 }
 </style>
